00b0ace8722200be03eab442a56919966f54bf40
Felipe Zanluca <felipezanluca@hotmail.com>
2025-01-26 14:51:07 -0300

    Criacao de um CalculoTeste3 para implementacao e teste da funcao BinaryOperator da documentacao de java.util

---
+++ src/lambdasFunction/testes/CalculoTeste3.java
@@ -0,0 +1,20 @@
+package lambdasFunction.testes;
+
+import java.util.function.BinaryOperator;
+
+/*
+Teste criado para a implementacao de "BinaryOperator<T>" de java.util.function da documentacao da oracle.
+ */
+
+public class CalculoTeste3 {
+
+    public static void main(String[] args) {
+
+        BinaryOperator<Double> calc = (x,y) -> x + y;
+        System.out.println("Soma: " + calc.apply(2.0, 3.0));
+
+        calc = (x,y) -> {return x * y;};
+        System.out.println("Multiplicacao: " + calc.apply(2.0, 3.0));
+
+    }
+}
